edeed6bd05fe818ead27200d790193e46278597d
Ihsan Arif Rahman <ihsan.android@gmail.com>
2015-09-10 20:51:05 +0700

    uas 2

--- CodeStrukturData/soal/UASP2.cpp
+++ CodeStrukturData/soal/UASP2.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
 
+using namespace std;
+
+int main(){
+    int temp[101]= {0};
+    int a,b,c,d,e,i,x,y,sum;
+
+    cin >> a >> b;
+    for(i=0;i<b;i++){
+        cin >> c >> d >> e;
+        temp[c]++;
+        temp[d]++;
+        temp[e]++;
+    }
+    cin >> x >> y;
+    sum = temp[x]+temp[y];
+    cout << sum << endl;
+
+
+    return 0;
+}
