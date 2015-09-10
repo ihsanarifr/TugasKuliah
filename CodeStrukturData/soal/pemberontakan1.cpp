c5649f7c3f2544a8d03df7146a6b6508b0543700
Ihsan Arif Rahman <ihsan.android@gmail.com>
2015-09-10 20:22:37 +0700

    uas 1 edit2

--- CodeStrukturData/soal/pemberontakan1.cpp
+++ CodeStrukturData/soal/pemberontakan1.cpp
@@ -6,29 +6,27 @@ using namespace std;
 
 int main(){
     int n,KotaA,KotaB,KotaC;
-    int count=0;
+    int bnyk=0;
     int i,j,k,l;
-
     cin >> n >> KotaA;
-
-    int bridge[N][N]={0};
-    int sum[N]={0};
+    int jembatan[N][N]={0};
+    int jumlah[N]={0};
 
     for(i=0;i<KotaA;i++){
         cin >> KotaB>> KotaC;
-        bridge[KotaB][KotaC]=bridge[KotaC][KotaB]=1;
+        jembatan[KotaB][KotaC]=jembatan[KotaC][KotaB]=1;
     }
 
     for(j=1;j<=n;j++){
         for(k=1;k<=n;k++){
-            count=count+bridge[j][k];
+            bnyk=bnyk+jembatan[j][k];
         }
-        sum[j]=count;
-        count=0;
+        jumlah[j]=bnyk;
+        bnyk=0;
     }
 
     for (l=1;l<=n;l++){
-        cout << sum[l] << endl;
+        cout << jumlah[l] << endl;
     }
-    return 0;
+return 0;
 }
